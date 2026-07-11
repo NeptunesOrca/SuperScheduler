dae73809065b8a3bbd6e34102d009779acd7be91
Joshua M <83213467+NeptunesOrca@users.noreply.github.com>
2026-07-11 00:34:41 -0600

    Recurrances can be calculated by providing the start *or* the end date

--- reccurance.py
+++ reccurance.py
@@ -2,10 +2,17 @@ from datetime import datetime, timedelta
 from time_management import parse_datetime
 
 class Reccurrance:
-    def __init__(self, start : datetime, duration : timedelta):
-        self.start = start
+    def __init__(self, date : datetime, duration : timedelta, useDateAsStart : bool = True):
+        self.start : datetime
+        self.end : datetime
         self.duration = duration
-        self.end = start + duration
+        if useDateAsStart:
+            self.start = date
+            self.end = date + duration
+        else:
+            self.end = date
+            self.start = date - duration
+
 
     def isExpired(self) -> bool:
         return datetime.now() > self.end
@@ -14,11 +21,13 @@ class Reccurrance:
     def from_dict(cls, payload:dict) -> Reccurrance:
         return cls(
             # Field   | Value                             | Default
-            start =     parse_datetime(payload["start"]),
+            date =     parse_datetime(payload["start"]),
             duration =  timedelta(seconds=payload.get("duration", 0))
+            # use date as start
         )
     
     def to_dict(self) -> dict:
+        # only provide the start date, end date will be calculated
         return {
             "start" : self.start.isoformat(),
             "duration": int(self.duration.total_seconds())
